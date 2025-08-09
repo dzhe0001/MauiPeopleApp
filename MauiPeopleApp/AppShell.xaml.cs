578e827f0b665a413484c36d6fed11afeac2d95d
Dmytro Dzhemchuk <dzhemchuk@gmail.com>
2025-08-09 16:25:46 -0400

    Implement biometrics

--- MauiPeopleApp/AppShell.xaml.cs
+++ MauiPeopleApp/AppShell.xaml.cs
@@ -5,6 +5,7 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute(nameof(Views.PersonListPage), typeof(Views.PersonListPage));
         Routing.RegisterRoute(nameof(Views.PersonDetailPage), typeof(Views.PersonDetailPage));
     }
 }
